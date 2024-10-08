2880c2a7de86dd6742cb668e30dcfb7842fea4c6
Gabriela-Abrego <jenyferramirez069@gmail.com>
2024-10-07 21:10:01 -0600

    Add files via upload
    Este proyecto es sobre el manejo de una tarjeta de credito, sobre deudas y pagos de un usuario.

---
+++ Persona.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio250824
+{
+    class Persona
+    {
+        //declaracion de las variables 
+        private int ID = 0;
+        private string Apellidos = "";
+        private string Nombres = "";
+        private int AñosNac = 0;
+        private string Genero = "";
+        private string NIT = "";
+        private int Deuda = 0;
+        private int limite = 0;
+
+        //creacion del constructor
+        public Persona ( int I, string Ap, string Nom, int AN, string Gn, string Nt, int Lim)
+        {
+            this.ID = I;
+            this.Apellidos = Ap;
+            this.Nombres = Nom;
+            this.AñosNac = AN;
+            this.Genero = Gn;
+            this.NIT = Nt;
+            this.limite = Lim; 
+        }
+        //creación de los metodos. 
+        //creación del metodo de compra 
+        public void Comprar (int precio)
+        {
+            if ((Deuda + precio) <= limite)
+            {
+                Deuda = Deuda + precio;
+            } else
+            {
+                Console.WriteLine("Error, Se execede el limite de credito.");
+                Console.WriteLine("Su saldo es de : {0}", Deuda);
+                Console.WriteLine("Su limite es: {0}", limite);
+            }
+        }
+        //creación del metodo de pagar 
+        public void Pagar (int pago)
+        {
+            if ((Deuda-pago) >= 0)
+            {
+                Deuda = Deuda - pago;
+            }
+            
+        }
+        //creación del método ver saldo
+        public void Saldo ()
+        {
+            Console.WriteLine("Su saldo es:" + Deuda );
+        }
+        //Creación del método ver la info del cliente 
+        public void VerInfo ()
+        {
+            Console.WriteLine("Nombre:" + Nombres);
+            Console.WriteLine("Apellido: {0}", Apellidos);
+            Console.WriteLine("ID: {0}", ID);
+            Console.WriteLine("Año de nacimiento:{0}", AñosNac);
+            Console.WriteLine("Género: {0}", Genero);
+            Console.WriteLine("NIT: {0}", NIT);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,146 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TarjetaPuntosEjercicio080924
+namespace ejercicio250824
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //creacion del vector 
-            TarjetaPuntos [] ListadoTarjeta = new TarjetaPuntos [25];
-            //guarda la elección del menu que haga el cliente.
-            int eleccion = 0;
-            //datos para crear la cuenta 
-            string Nombre = "";
-            string Nocuentas = "";
-            //esta variable es el contador que lleva el registro de la posiciones del vector
-            int posiciones = 0;
-            //para ir recorriendo las posiciones y buscando los no. de cuentas. 
-            int  Var2 = -1;
+            //crear dos cliente, que hagan dos compras y dos pagos y ver el saldo 
+            //instanciacion del objeto 
+            Persona ObjetoGabriela  = new Persona(888, "Abrego", "Gabriela", 2002, "femenino", "12345", 5000);
+            Persona ObjetoJenyfer = new Persona(999, "Ramirez", "Jenyfer", 2002, "femenino", "5678", 4500);
 
-            //Menú
-            do
-            {
-                Console.WriteLine("_______________________________________________________________");
+            ObjetoGabriela.Comprar(250);
+            ObjetoGabriela.Comprar(750);
+            ObjetoGabriela.Pagar(600);
+            Console.WriteLine("El saldo de Gabriela es:");
+            ObjetoGabriela.Saldo();
 
-                Console.WriteLine("Bienvenido a Tu Tarjeta de Puntos");
-                Console.WriteLine("A continuación se presenta un menú");
-                Console.WriteLine("Por favor elija la opcion que más se adecua a sus nececidades.");
-                Console.WriteLine("1. Crear");
-                Console.WriteLine("2. Modificar");
-                Console.WriteLine("3. Eliminar"); 
-                Console.WriteLine("4. Compras");
-                Console.WriteLine("5. Ver Tarjeta");
-                Console.WriteLine("6. ver Listado de Tarjetas");
-                Console.WriteLine("por favor ingrese su eleción:");
-                eleccion = Convert.ToInt32(Console.ReadLine());
-                Console.Clear();
-                Console.WriteLine("¡Gracias!");
+            ObjetoJenyfer.Comprar(500);
+            ObjetoJenyfer.Comprar(700);
+            ObjetoJenyfer.Pagar(1000);
+            Console.WriteLine("El saldo de Jenyfer es:");
+            ObjetoJenyfer.Saldo();
 
-                if (eleccion ==1)
-                {
-                    //el usuario a elegido crear cuenta 
-                    Console.WriteLine("_______________________________________________________________");
-                    Console.WriteLine("Bienvenido a la creacion de tu Tarjeta de puntos:");
-                    Console.WriteLine("Por favor Proporciona los datos que se te piden ");
-                    Console.WriteLine("Ingrese su nombre:");
-                    Nombre = Console.ReadLine();
-                    Console.WriteLine("Ingrese su numero de cuenta ");
-                    Nocuentas = Console.ReadLine();
-                    //asignar espacio en el vector 
-                    ListadoTarjeta [posiciones] = new TarjetaPuntos(Nocuentas,Nombre);
-                    posiciones++;
-                }
-                else if (eleccion ==2)
-                {
-                    Console.WriteLine("_______________________________________________________________");
-                    Console.WriteLine("Bienvenido a la opcion Modificar de tu Tarjeta de puntos:");
-                    Console.WriteLine("Ingrese su No. de cuenta por favor:");
-                    Nocuentas = Console.ReadLine();
-                    //1. buscar la pocisión
-                    for (int i = 0; i < posiciones; i++)
-                    {
-                        if (ListadoTarjeta[i].VERCUENTA()==Nocuentas)
-                        {
-                            Var2 = i;
-                        }
-                    }
-                    //mostrar la información original 
-                    Console.WriteLine("");
-                    Console.WriteLine("La información ingresada anteriormente es: ");
-                    ListadoTarjeta[Var2].VerInformación();
-                    //ingreso de información nueva 
-                    //el usuario solo puede modificar su nombre. 
-                    Console.WriteLine("");
-                    Console.WriteLine("Ingrese el nuevo nombre que aparecera en su tarjeta de puntos:");
-                    Nombre = Console.ReadLine();
-                    //se llama al método correpondiente a la acción modificar  
-                    ListadoTarjeta[Var2].Modificar(Nombre);
-
-                    //mostramos la nueva informacion. 
-                    ListadoTarjeta[Var2].VerInformación();
-                }
-                else if (eleccion ==3)
-                {
-                    Console.WriteLine("_______________________________________________________________");
-                    Console.WriteLine("Bienvenido a la Opcion Eliminar de tu Tarjeta de puntos:");
-                    Console.WriteLine("Ingrese su No. de cuenta por favor:");
-                    Nocuentas = Console.ReadLine();
-                    //1. buscar la pocisión
-                    for (int i = 0; i < posiciones; i++)
-                    {
-                        if (ListadoTarjeta[i].VERCUENTA() == Nocuentas)
-                        {
-                            Var2 = i;
-                        }
-                    }
-                    //se llama al metodo eliminar 
-                    ListadoTarjeta[Var2].Eliminar(Nocuentas);
-                }
-                else if (eleccion ==4)
-                {
-                    Console.WriteLine("_______________________________________________________________");
-                    Console.WriteLine("Bienvenido a la opción de comprar de tu Tarjeta de puntos:");
-                    Console.WriteLine("Por favor ingrese el monto de su compra:");
-                    int MontoCompra = Convert.ToInt32(Console.ReadLine());
-                    for (int i = 0; i < posiciones; i++)
-                    {
-                        if (ListadoTarjeta[i].VERCUENTA() == Nocuentas)
-                        {
-                            Var2 = i;
-                        }
-                    }
-                    //se llama al metodo de compra 
-                    ListadoTarjeta[Var2].Comprar(MontoCompra);
-                }
-                else if (eleccion == 5)
-                {
-                    Console.WriteLine("_______________________________________________________________");
-                    Console.WriteLine("Bienvenido a la opción Ver tarjeta de tu Tarjeta de puntos:");
-                    //se llama al metodo ver cuenta donde podemos ver toda la informacion del usuario. 
-                    for (int i = 0; i < posiciones; i++)
-                    {
-                        if (ListadoTarjeta[i].VERCUENTA() == Nocuentas)
-                        {
-                            Var2 = i;
-                        }
-                    }
-                    ListadoTarjeta[Var2].VerInformación();
-                }
-                else if (eleccion ==6)
-                {
-                    Console.WriteLine("_______________________________________________________________");
-                    Console.WriteLine("Bienvenido a la opción ver Listado de Tarjetas de puntos:");
-                    for (int j = 0; j < posiciones; j++)
-                    {
-                        Console.WriteLine(ListadoTarjeta[j].VERCUENTA());
-                    }
-                }
-            } while (eleccion <=6);
-            Console.WriteLine("La opción ingresada no se encuentra en el menú.");
             Console.ReadKey();
-            
         }
     }
 }
